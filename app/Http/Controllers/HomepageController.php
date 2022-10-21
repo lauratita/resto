9f1ac456d12719e58b3e4ffa38b7bade2cbf6233
Seooo <stevenyonathan88@gmail.com>
2022-10-21 12:24:01 +0700

    Menghapus DomPDF

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -3,13 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Menu;
-use App\Models\Gallery;
-// use Barryvdh\DomPDF\PDF;
 use App\Models\Blog;
 use App\Models\Order;
+use App\Models\Gallery;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use PDF;
 
 class HomepageController extends Controller
 {
@@ -67,29 +65,6 @@ class HomepageController extends Controller
             'active' => 'gallery',
             'galleries' => Gallery::all()
         ]);
-    }
-    public function cetak_pdf()
-    {
-        $menu_food = DB::select("select * from menus where category = 'Food'");
-        $menu_drink = DB::select("select * from menus where category = 'Drink'");
-        $menu_dessert = DB::select("select * from menus where category = 'Dessert'");
-        $menu_package = DB::select("select * from menus where category = 'Package'");
-
-        return view('homepage.menu_pdf', [
-            'menus' => Menu::all(),
-            'menus_food' => $menu_food,
-            'menus_drink' => $menu_drink,
-            'menus_dessert' => $menu_dessert,
-            'menus_package' => $menu_package
-        ]);
-        // $pdf = PDF::loadview('homepage.menu_pdf', [
-        //     'menus' => Menu::all(),
-        //     'menus_food' => $menu_food,
-        //     'menus_drink' => $menu_drink,
-        //     'menus_dessert' => $menu_dessert,
-        //     'menus_package' => $menu_package
-        // ])->setOptions(['defaultFont' => 'sans-serif']);
-        // return $pdf->download('Menu-Resto.pdf');
     }
     public function story()
     {
